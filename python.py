39f15ad9b9703d5a9838a9f92f1d4da068027f48
patrikarfalguni-star <patrikarfalguni@gmail.com>
2026-03-10 15:51:21 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,62 @@
+# find ypur current age
+birth_year=int(input("enter your birth year:"))
+current_year=2026
+age=current_year-birth_year
+print("age=",age)
+# swap two numbers
+a=10
+b=20
+a,b=b,a
+print(a)
+print(b)
+#convert km to meters 1000
+#convert minits to hours 60
+#find sqr and cube
+num=10
+sqr=num*num
+cube=num*num*num
+print(sqr)
+#calculate simple interest
+#si=(p*r*t)/100
+p=float(input("enter principle amt:"))
+r=float(input("enter rate:"))
+t=float(input("enter time"))
+si=(p*r*t)/100
+print("simple interest =",si)
+
+#calculate area of circle,rectangle
+#area_of_rect=length*width
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+3
